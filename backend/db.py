cfc70e6b864ce26e29f447e04f8ed398455c08ad
YolloPlays <spexe0011@gmail.com>
2025-07-02 02:38:35 +0200

    persistent

--- backend/db.py
+++ backend/db.py
@@ -1,4 +1,6 @@
 from sqlmodel import create_engine
+from pathlib import Path
 
+(Path('.') / "data").mkdir(parents=True, exist_ok=True)
 DATABASE_URL = "sqlite:///data/database.db"
 engine = create_engine(DATABASE_URL)
