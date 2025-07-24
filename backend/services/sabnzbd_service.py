6d9cc8a3d6d14e141c86cf5252fe11956c01b5e2
YolloPlays <spexe0011@gmail.com>
2025-07-24 08:44:43 +0200

    replaced requests with httpx

--- backend/services/sabnzbd_service.py
+++ backend/services/sabnzbd_service.py
@@ -1,5 +1,5 @@
 from backend.exceptions import NzbsError
-import requests
+import httpx
 from uuid import uuid4
 from backend.config import ConfigManager
 
@@ -12,7 +12,7 @@ def indexer_search(q: str, cfg: ConfigManager):
         "q": q,
         "apikey": cfg.indexer_apikey
     }
-    response = requests.get(cfg.indexer_url, params=search)
+    response = httpx.get(cfg.indexer_url, params=search)
     if response.status_code != 200: raise NzbsError(status_code=response.status_code, detail=response.text)
     response.encoding = 'utf-8'
     if "error" in response.text: raise NzbsError(status_code=403, detail=response.text)
@@ -26,18 +26,18 @@ def indexer_nzb(guid: str, cfg: ConfigManager):
         "o" : "json",
         "apikey": cfg.indexer_apikey
     }
-    response = requests.get(cfg.indexer_url, params=get)
+    response = httpx.get(cfg.indexer_url, params=get)
     if response.status_code != 200: raise NzbsError(status_code=response.status_code, detail=response.text)
     response.encoding = 'utf-8'
     if "error" in response.text: raise NzbsError(status_code=403, detail=response.text)
     return response.content
 
 def download(nzb : bytes, cfg: ConfigManager, nzbname: str = str(uuid4())):
-    resp=requests.post(cfg.downloader_url, params={"apikey": cfg.downloader_apikey}, data={"mode": "addfile", "nzbname": nzbname, "cat": cfg.downloader_category or ""}, files={"nzbfile": nzb})
+    resp=httpx.post(cfg.downloader_url, params={"apikey": cfg.downloader_apikey}, data={"mode": "addfile", "nzbname": nzbname, "cat": cfg.downloader_category or ""}, files={"nzbfile": nzb})
     if resp.status_code != 200: raise NzbsError(status_code=resp.status_code, detail=resp.text)
 
 def get_config(cfg: ConfigManager, section: str, keyword: str = None) -> dict:
-    resp = requests.get(cfg.downloader_url, params={"apikey":cfg.downloader_apikey,"mode":"get_config","section":section,"output":"json"})
+    resp = httpx.get(cfg.downloader_url, params={"apikey":cfg.downloader_apikey,"mode":"get_config","section":section,"output":"json"})
     if resp.status_code != 200: raise NzbsError(status_code=resp.status_code, detail=resp.text)
     parsed = resp.json()
     if keyword:
