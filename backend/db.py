70c093a81f32d234ae2a393ca652b60355ea9d45
YolloPlays <spexe0011@gmail.com>
2025-07-24 08:39:03 +0200

    changed back to true bind mounts for docker

--- backend/db.py
+++ backend/db.py
@@ -1,6 +1,6 @@
 from sqlmodel import create_engine
 from pathlib import Path
 
-Path('./config').mkdir(parents=True, exist_ok=True)
-DATABASE_URL = "sqlite:///config/database.db"
+Path('/config').mkdir(parents=True, exist_ok=True)
+DATABASE_URL = "sqlite:////config/database.db"
 engine = create_engine(DATABASE_URL)
